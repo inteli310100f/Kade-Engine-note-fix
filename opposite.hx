bb4c76e3fdeb3504577cc7b42181c2c4d58c8c6a
tmpfs 💿 <usernametmpfs@gmail.com>
2024-12-08 18:50:56 -0300

    Create opposite.hx
    supposed to do the opposite, didn't test yet

---
+++ opposite.hx
@@ -0,0 +1,13 @@
+// evil!
+
+if (unspawnNotes.length > 0) {
+    var i:Int = 0;
+    while (true) { // infinite loop, yay! i fucking hate myself!
+        if (unspawnNotes[i].strumTime - Conductor.songPosition < 3500) {
+            var dunceNote:Note = unspawnNotes[i];
+            notes.add(dunceNote);
+            unspawnNotes.splice(i, 1);
+        }
+        i++; // always increment, even if an element was removed
+    }
+}
