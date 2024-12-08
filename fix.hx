4e5703f80f68335d690efd202297d7efbc524e23
tmpfs 💿 <usernametmpfs@gmail.com>
2024-12-08 19:20:33 -0300

    Update fix.hx
    should be more efficient and have more perfomance

--- fix.hx
+++ fix.hx
@@ -1,13 +1,12 @@
-if (unspawnNotes.length > 0) {
-			var i:Int = 0; // start index
-			while (i < unspawnNotes.length) {
-				if (unspawnNotes[i].strumTime - Conductor.songPosition < 3500) {
-					var dunceNote:Note = unspawnNotes[i];
-					notes.add(dunceNote);
-					unspawnNotes.splice(i, 1); // remove the note at index i
-					// don't increment i because the list shifts left aftre splicing
-				} else {
-					i++; // only move to the next index if no note was removed
+		if (unspawnNotes.length > 0) {
+			var thresholdTime = Conductor.songPosition + 3500;
+			var i = unspawnNotes.length - 1; // start from the end
+		
+			while (i >= 0) {
+				if (unspawnNotes[i].strumTime < thresholdTime) {
+					notes.add(unspawnNotes[i]);
+					unspawnNotes.splice(i, 1);
 				}
+				i--;
 			}
 		}
