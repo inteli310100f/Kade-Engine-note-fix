8e64bd23d4612234f2bbbe82fb65f7a723776b5a
inteli310100f <hiddenvoice67@gmail.com>
2025-01-22 04:19:52 -0300

    Update fix.hx

--- fix.hx
+++ fix.hx
@@ -1,12 +1,9 @@
-		if (unspawnNotes.length > 0) {
-			var thresholdTime = Conductor.songPosition + 600;
+if (unspawnNotes.length > 0) {
 			var i = unspawnNotes.length - 1; // start from the end
 		
 			while (i >= 0) {
-				if (unspawnNotes[i].strumTime < thresholdTime) {
-					notes.add(unspawnNotes[i]);
-					unspawnNotes.splice(i, 1);
-				}
+				notes.add(unspawnNotes[i]);
+				unspawnNotes.splice(i, 1);
 				i--;
 			}
 		}
